089e943b7e822d70d6c9a2019b7273fab29ecf3c
Andrew Tritt <ajtritt@lbl.gov>
2017-11-21 11:37:07 -0800

    remove global distance counter

--- exabiome/mg_sampler/sample.c
+++ exabiome/mg_sampler/sample.c
@@ -10,9 +10,7 @@ typedef struct tdist {
     int len;
 } tdist;
 
-double* cum_dist;
-
-tdist* leaf_distance(NODE * a) {
+tdist* leaf_distance(NODE * a, double * cum_dist) {
 
     tdist* ret = (tdist*) malloc(sizeof(tdist));
     if (a->node_id > -1){
@@ -25,9 +23,9 @@ tdist* leaf_distance(NODE * a) {
         tdist* l_dist;
         tdist* r_dist;
         //#pragma omp task shared(l_dist)
-        l_dist = leaf_distance(a->left);
+        l_dist = leaf_distance(a->left, cum_dist);
         //#pragma omp task shared(r_dist)
-        r_dist = leaf_distance(a->right);
+        r_dist = leaf_distance(a->right, cum_dist);
         //#pragma omp taskwait
         ret->len = l_dist->len + r_dist->len;
         ret->id = (int*) malloc(sizeof(int)*ret->len);
@@ -57,66 +55,10 @@ tdist* leaf_distance(NODE * a) {
     return ret;
 }
 
-int nleaves;
-
-NODE* read_node(char * nwk, int * idx) {
-    char *name;
-    char *blen;
-    size_t name_n = 0;
-    size_t blen_n = 0;
-
-    int read_blen = 0;
-
-    name = malloc(100);
-    blen = malloc(100);
-
-    NODE * ret = (NODE *) malloc(sizeof(NODE));
-
-    while (nwk[*idx] != '\0') {
-        if (nwk[*idx] == '(') { // left parentheses -->
-            (*idx)++;
-            ret->left = read_node(nwk, idx);
-        }
-        else if (nwk[*idx] == ')') { // right parentheses --> done reading branch length
-            blen[blen_n] = '\0';
-            ret->blen = atof(blen);
-            read_blen = 0;
-            (*idx)++;
-            break;
-       } else if (nwk[*idx] == ','){ // comma --> add branch length to ret
-            if (read_blen){
-                blen[blen_n] = '\0';
-                ret->blen = atof(blen);
-            } else {
-                (*idx)++;
-                ret->right = read_node(nwk, idx);
-            }
-            break;
-        } else if (nwk[*idx] == ':') {  // colon --> finished reading contents of node, next up is branch length
-            if (name_n > 0){   // we were reading a leaf node
-                name[name_n] = '\0';
-                nleaves++;
-                ret->name = name;
-            }
-            read_blen = 1;
-            (*idx)++;
-        } else {
-            if (read_blen) {
-                blen[blen_n++] = nwk[*idx];
-            } else {
-                name[name_n++] = nwk[*idx];
-            }
-            (*idx)++;
-        }
-    }
-    return ret;
-}
-
-
 int main() {
     // allocate array for cumulative distances
     int n_nodes = 3;
-    cum_dist = (double *) malloc(sizeof(double)*n_nodes);
+    double * cum_dist = (double *) malloc(sizeof(double)*n_nodes);
     int i;
     for (i = 0; i < n_nodes; i++){
         cum_dist[i] = 0;
@@ -128,23 +70,23 @@ int main() {
     AB = (NODE) {.node_id = -1, .blen = 0.5, .left = &A, .right = &B};
     ROOT = (NODE) {.node_id = -2, .blen = -0.1, .left=&C, .right=&AB};
 
-    leaf_distance(&ROOT);
+    leaf_distance(&ROOT, cum_dist);
     printf("Done computing total distances\n");
     printf("A = %.3f, should be 2.200\n", cum_dist[0]);   // A = 2.200
     printf("B = %.3f, should be 2.300\n", cum_dist[1]);   // B = 2.300
     printf("C = %.3f, should be 3.500\n", cum_dist[2]);   // C = 3.500
 
-    nleaves = 0;
-    char * tree = (char*) malloc(26*sizeof(char));
-    strcpy(tree, "(C:1.0,(B:0.3,A:0.2):0.5)\0");
-    int idx = 0;
-    int * idx_ptr = &idx;
-    NODE * READ_ROOT = read_node(tree, idx_ptr);
-
-    printf("Compute total distances from read tree\n");
-    leaf_distance(READ_ROOT);
-    printf("Done computing total distances\n");
-    printf("A = %.3f, should be 2.200\n", cum_dist[0]);   // A = 2.200
-    printf("B = %.3f, should be 2.300\n", cum_dist[1]);   // B = 2.300
-    printf("C = %.3f, should be 3.500\n", cum_dist[2]);   // C = 3.500
+//    nleaves = 0;
+//    char * tree = (char*) malloc(26*sizeof(char));
+//    strcpy(tree, "(C:1.0,(B:0.3,A:0.2):0.5)\0");
+//    int idx = 0;
+//    int * idx_ptr = &idx;
+//    NODE * READ_ROOT = read_node(tree, idx_ptr);
+//
+//    printf("Compute total distances from read tree\n");
+//    leaf_distance(READ_ROOT);
+//    printf("Done computing total distances\n");
+//    printf("A = %.3f, should be 2.200\n", cum_dist[0]);   // A = 2.200
+//    printf("B = %.3f, should be 2.300\n", cum_dist[1]);   // B = 2.300
+//    printf("C = %.3f, should be 3.500\n", cum_dist[2]);   // C = 3.500
 }
