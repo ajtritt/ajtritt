0bfde7d8823856929a25eba30cabc5a67145e612
Andrew Tritt <ajtritt@lbl.gov>
2017-11-29 00:11:16 -0800

    added generation of weights

--- exabiome/mg_sampler/sample_tree.c
+++ exabiome/mg_sampler/sample_tree.c
@@ -1,66 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <math.h>
-#include <string.h>
+//#include <string.h>
 
-#define INTERNAL_NODE -1
-
-struct NODE {
-    struct NODE * left;
-    struct NODE * right;
-    int node_id;
-    double blen;
-    char* name;
-};
-
-struct TREE {
-    struct NODE * root;
-    int nleaves;
-    char ** names;
-};
-
-struct TREE * read_node_rec(char ** nwk_ptr, int * id) {
+void read_tree(char ** nwk_ptr, double * curr_dist, double * weights, char ** names, int * id) {
     char * nwk = *nwk_ptr;
     char *name = malloc(256), *blen = malloc(256);
     size_t name_n = 0, blen_n = 0;
 
-    struct NODE * root = (struct NODE *) malloc(sizeof(struct NODE));
-    root->left = NULL;
-    root->right = NULL;
-    root->node_id = -1;
     int nleaves = 0;
 
     int read_blen = 0;
     char c = *nwk;
+    int s = *id;
+    int m = *id;
     while (c != '\0') {
         if (c == '(') { // read left node
             nwk++;
-            struct TREE * subtree = read_node_rec(&nwk, id);
-            root->left = subtree->root;
-            nleaves += subtree->nleaves;
+            read_tree(&nwk, curr_dist, weights, names, id);
+            m = *id;
         } else if (c == ')') { // done reading branch length
             blen[blen_n] = '\0';
-            root->blen = atof(blen);
             read_blen = 0;
             nwk++;
             break;
        } else if (c == ','){
             if (read_blen){ // add branch length to ret
                 blen[blen_n] = '\0';
-                root->blen = atof(blen);
                 break;
             } else {       // read right node
                 nwk++;
-                struct TREE * subtree = read_node_rec(&nwk, id);
-                root->right = subtree->root;
-                nleaves += subtree->nleaves;
+                read_tree(&nwk, curr_dist, weights, names, id);
             }
         } else if (c == ':') {  // read branch length
             if (name_n > 0){   // we were reading a leaf node
                 name[name_n] = '\0';
                 nleaves = 1;
-                root->name = name;
-                root->node_id = (*id)++;
+                names[(*id)++] = name;
             }
             read_blen = 1;
             nwk++;
@@ -74,126 +51,26 @@ struct TREE * read_node_rec(char ** nwk_ptr, int * id) {
         }
         c = *nwk;
     }
+    int e = *id;
+    assert( m <  e);
     (*nwk_ptr) = nwk;
-    struct TREE * ret = (struct TREE *) malloc(sizeof(struct TREE));
-    ret->root = root;
-    ret->nleaves = nleaves;
+    int i, j;
+    double tmp;
+    for (i = s; i < m; i++){
+        for (j = m; j < e; j++){
+            tmp = curr_dist[i] + curr_dist[j];
+            weights[i] += tmp;
+            weights[j] += tmp;
+        }
+    }
+    tmp = atof(blen);
+    for (i = s; i < m; i++) {
+        curr_dist[i] += tmp;
+    }
+    for (i = m; i < e; i++) {
+        curr_dist[i] += tmp;
+    }
     free(blen);
-    return ret;
-}
-
-struct TREE * read_node(char * nwk) {
-    int id = 0;
-    struct TREE * ret = read_node_rec(&nwk, &id);
-    return ret;
-}
-
-void get_names(struct NODE * node, char ** names){
-    if (!node->left){
-        names[node->node_id] = node->name;
-    } else {
-        node->node_id = -1;
-        get_names(node->left, names);
-        get_names(node->right, names);
-    }
-}
-
-struct TREE * read_tree(char * nwk){
-    struct TREE * ret = read_node(nwk);
-    ret->names = (char**)malloc(ret->nleaves*sizeof(char*));
-    get_names(ret->root, ret->names);
-    return ret;
-}
-
-void print_leaves(struct NODE * node){
-    if (!node->left){
-        printf("%s:%.3f id = %d\n", node->name, node->blen, node->node_id);
-    } else {
-        printf("internode: blen = %0.3f\n", node->blen);
-        print_leaves(node->left);
-        print_leaves(node->right);
-    }
-}
-
-typedef struct tdist {
-    int* id;
-    double* dist;
-    int len;
-} tdist;
-
-tdist* leaf_distance(struct NODE * a, double * cum_dist) {
-
-    tdist* ret = (tdist*) malloc(sizeof(tdist));
-    if (a->node_id == INTERNAL_NODE){
-        tdist* l_dist;
-        tdist* r_dist;
-        l_dist = leaf_distance(a->left, cum_dist);
-        r_dist = leaf_distance(a->right, cum_dist);
-        ret->len = l_dist->len + r_dist->len;
-        ret->id = (int*) malloc(sizeof(int)*ret->len);
-        ret->dist = (double*) malloc(sizeof(double)*ret->len);
-        int i;
-        for (i = 0; i < l_dist->len; i++) { // copy left leaf distances
-            ret->id[i] = l_dist->id[i];
-            ret->dist[i] = l_dist->dist[i] + a->blen;
-        }
-        for (i = 0; i < r_dist->len; i++) { // copy right leaf distances
-            ret->id[i+l_dist->len] = r_dist->id[i];
-            ret->dist[i+l_dist->len] = r_dist->dist[i] + a->blen;
-        }
-        // tally distances
-        int j;
-        double dist_ij;
-        for (i = 0; i < l_dist->len; i++) {
-            for (j = 0; j < r_dist->len; j++) {
-                dist_ij = l_dist->dist[i] + r_dist->dist[j];
-                cum_dist[l_dist->id[i]] += dist_ij;
-                cum_dist[r_dist->id[j]] += dist_ij;
-            }
-        }
-        free(l_dist);
-        free(r_dist);
-    } else {
-        ret->id = (int*) malloc(sizeof(int));
-        ret->dist = (double*) malloc(sizeof(int));
-        ret->id[0] = a->node_id;
-        ret->dist[0] = a->blen;
-        ret->len = 1;
-    }
-    return ret;
-}
-
-double * compute_total_distances(struct TREE * tree) {
-    double * ret = (double *) malloc((tree->nleaves)*sizeof(double));
-    int i;
-    for (i = 0; i < tree->nleaves; i++){
-        ret[i] = 0.0;
-    }
-    leaf_distance(tree->root, ret);
-    return ret;
-}
-
-
-struct TREE * read_nwk_tree(char *fileName) {
-    FILE *file = fopen(fileName, "r");
-    char *code;
-    size_t n = 0;
-    int c;
-
-    if (file == NULL) return NULL; //could not open file
-    fseek(file, 0, SEEK_END);
-    long f_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    code = malloc(f_size);
-
-    while ((c = fgetc(file)) != EOF) {
-        if (c == 10 || c == 32 || c == 9) {
-            continue;
-        }
-        code[n++] = (char)c;
-    }
-    code[n] = '\0';
-    return read_tree(code);
 }
 
 int binsearch(double * weights, int l, int r, double val){
@@ -205,12 +82,6 @@ int binsearch(double * weights, int l, int r, double val){
     } else {
         return binsearch(weights, mid+1, r, val);
     }
-}
-
-int sample(double * weights, int len) {
-    double U = (double) rand()/RAND_MAX;
-    U *= weights[len-1];
-    return binsearch(weights, 0, len, U);
 }
 
 void shift_weights(double * cum, int idx, int len) {
@@ -226,24 +97,60 @@ void shift_weights(double * cum, int idx, int len) {
 }
 
 int main(int argc, char ** argv) {
-
     if (argc != 3) {
         printf("Usage: sample_tree in.nwk n_samples\n");
         return -1;
     }
 
-    printf("Reading a second time\n");
-    struct TREE * tree2 = read_nwk_tree(argv[1]);
-    int nleaves = tree2->nleaves;
-    int nsamples = atoi(argv[2]);
-    struct NODE * root2 = tree2->root;
+    FILE *file = fopen(argv[1], "r");
+    char *code;
+    size_t n = 0;
+    int c;
 
-    printf("Computing distances on tree (%d leaves)\n", nleaves);
-    double * weights = (double*) malloc(sizeof(double)*nleaves);
+    printf("Reading tree\n");
+    if (file == NULL) return 1; //could not open file
+    // compute file size
+    fseek(file, 0, SEEK_END);
+    long f_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    code = malloc(f_size);
+
+    int nsamples = atoi(argv[2]);
+
+    // read in newick string
+    int internodes_n = 0;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == 10 || c == 32 || c == 9) {
+            continue;
+        }
+        if (c == 44)
+            internodes_n++;
+        code[n++] = (char)c;
+    }
+    code[n] = '\0';
+
+    // allocate distance and weights
+    int nleaves = internodes_n+1;
+    if (nsamples > nleaves){
+        printf("nsamples must be fewer than the number of leaves (%d)\n", nleaves);
+        return 1;
+    }
+    double * curr_dist = (double *) malloc(sizeof(double)*(nleaves));
+    double * weights = (double *) malloc(sizeof(double)*(nleaves));
+    char ** names = (char **) malloc(sizeof(char*)*(nleaves));
     int i;
-    for (i = 0; i < nleaves; i++)
+    for (i = 0; i <= internodes_n; i++){
+        curr_dist[i] = 0.0;
         weights[i] = 0.0;
-    leaf_distance(root2, weights);
+    }
+
+    // parse newick string and compute weights
+    i = 0;
+    int * id = &i;
+    printf("Computing distances on tree (%d leaves)\n", nleaves);
+    read_tree(&code, curr_dist, weights, names, id);
+
+    // compute cumulative weights
     double * cum_weights = (double*) malloc(sizeof(double)*nleaves);
     double sum = 0.0;
     for (i = 0; i < nleaves; i++){
@@ -251,10 +158,13 @@ int main(int argc, char ** argv) {
         cum_weights[i] = sum;
     }
 
+    // allocate node ids to keep track of samples
     int * node_ids = (int*) malloc(nleaves*sizeof(int));
     for (i = 0; i < nleaves; i++){
         node_ids[i] = i;
     }
+
+    // sample leaves
     int idx;
     int rem_leaves = nleaves;
     double U;
@@ -270,8 +180,19 @@ int main(int argc, char ** argv) {
         rem_leaves--;
     }
 
-    printf("printing samples\n");
+    // compute read abundance
+    double * abundance = (double *) malloc(sizeof(double)*nsamples);
+    sum = 0.0;
+    for (i = 0; i < nsamples; i++){
+        abundance[i] = -log(-((double) rand())/RAND_MAX + 1);
+        sum += abundance[i];
+    }
+    for (i = 0; i < nsamples; i++)
+        abundance[i] = abundance[i]/sum;
+
+    printf("Printing samples\n");
+    printf("img_taxon_oid\tabundance\n");
     for (i = rem_leaves; i < nleaves; i++){
-        printf("%s\n", tree2->names[node_ids[i]]);
+        printf("%s\t%0.12f\n", names[node_ids[i]], abundance[i-rem_leaves]);
     }
 }
