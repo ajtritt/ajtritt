d7dca2a5fd83e27e8ecd9615a30007b2876ffef5
Andrew Tritt <ajtritt@lbl.gov>
2019-10-08 15:27:42 -0700

    finish install script

--- conf/install.sh
+++ conf/install.sh
@@ -1,1 +1,16 @@
 cp -r .gitconfig .inputrc .tmux .tmux.conf .vimrc $HOME
+
+bash_config="$HOME/.bash_config"
+if [ ! -d "$bash_config" ]; then
+    echo "Making $bash_config"
+    mkdir $bash_config
+fi
+
+cp bash/* ~/.bash_config/
+
+tmpfile=$(mktemp /tmp/ajtritt.XXXXXX)
+
+echo "for sh in $bash_config/*; do source \$sh done" > $tmpfile
+cat $HOME/.bash_profile >> $tmpfile
+cp $tmpfile $HOME/.bash_profile
+rm $tmpfile
