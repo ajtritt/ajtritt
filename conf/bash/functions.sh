86dc428947e96f76b13ce94c8fd3a28b80e5a694
Andrew Tritt <ajtritt@lbl.gov>
2019-10-08 15:14:53 -0700

    update functions.sh

--- conf/bash/functions.sh
+++ conf/bash/functions.sh
@@ -1,6 +1,47 @@
+alias tl='tmux ls'
+alias ta='tmux attach -t'
+alias tn='tmux new -s'
+
 function findgrep ()
 {
-    local wc=${1:?"Missing wildcard pattern"};
-    local pat=${2:?"Missing pattern to grep for"};
-    find . -name "$wc" -exec grep -Hn $pat {} \;
+    local dir=${1:?"Missing directory"};
+    local wc=${2:?"Missing wildcard pattern"};
+    local pat=${3:?"Missing pattern to grep for"};
+    find $dir -name "$wc" -exec grep -Hn $pat {} \;
+}
+
+function colnames ()
+{
+    local path=${1:?"Missing CSV"};
+    local delim=${2:-","};
+    head -n1 $path | tr $delim '\n' | nl
+}
+
+function start_jupyter ()
+{
+    tmux new-session -d -s jupyter -n run
+    tmux send-keys -t jupyter:run "cd ~/projects" Enter
+    tmux send-keys -t jupyter:run "jupyter lab" Enter
+}
+
+function transpos ()
+{
+
+    local path=${1:?"Missing file"};
+    awk '
+    { 
+        for (i=1; i<=NF; i++)  {
+            a[NR,i] = $i
+        }
+    }
+    NF>p { p = NF }
+    END {    
+        for(j=1; j<=p; j++) {
+            str=a[1,j]
+            for(i=2; i<=NR; i++){
+                str=str" "a[i,j];
+            }
+            print str
+        }
+    }' $path
 }
