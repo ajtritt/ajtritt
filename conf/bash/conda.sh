717457ad9d356181a4db3676be78c0029200654b
Andrew Tritt <ajtritt@lbl.gov>
2020-01-09 15:30:33 -0800

    remove envvar export

--- conf/bash/conda.sh
+++ conf/bash/conda.sh
@@ -5,20 +5,19 @@ function new_env()
     local pyvers=${2:?"Please provide Python version"}
     shift
     shift
-    
-    conda create -n $envname python=$pyvers
+
+    conda create -y -n $envname python=$pyvers
     conda activate $envname
-    conda install -n $envname -c clawpack -c conda-forge hdf5-parallel
-    conda install -n $envname -c conda-forge mpich
-    conda install -n $envname -c conda-forge mpi4py
-    conda install -n $envname -c conda-forge gxx_linux-64
-    mkdir $HOME/mytmp
-    export TMPDIR=$HOME/mytmp
-    export CC=mpicc
-    export HDF5_MPI="ON"
-    pip install --no-binary=h5py h5py
+    conda install -y -n $envname -c clawpack -c conda-forge hdf5-parallel
+    conda install -y -n $envname -c conda-forge mpich
+    conda install -y -n $envname -c conda-forge mpi4py
+    conda install -y -n $envname -c conda-forge gxx_linux-64
+    CC=mpicc HDF5_MPI="ON" pip install --no-binary=h5py h5py
 
     for pkg in $@; do
         pip install $pkg
     done
+
+    conda deactivate
+    echo "Done building conda environment $envname"
 }
