c1353de0d9a0879424b5e1fc96622b7f5d6a8599
Andrew Tritt <ajtritt@lbl.gov>
2022-04-26 11:34:51 -0700

    include QOS in sqme output

--- conf/bash/systems/cori.sh
+++ conf/bash/systems/cori.sh
@@ -1,4 +1,5 @@
-alias sqme="squeue -u ajtritt"
-alias cscratch="cd $CSCRATCH"
+alias sqme="squeue -u ajtritt -o \"%.18i %.9P %.20j %.8u %.8T %.10M %.9l %.6D %R %q\""
+alias bjobs="squeue -u ajtritt"
+alias scratch="cd $SCRATCH"
 
 alias findtouch="find . -exec touch -a {} \;"
